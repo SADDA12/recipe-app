cf2dbc92e6176112885a4816755661bcf4216c2b
SADDA12 <sammiedaga@gmail.com>
2024-02-27 12:26:04 +0000

    took API key to env file, enabled searching recipes and implemented enter key with onKeyDown effect

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,7 @@ import {AppBar, Toolbar, Typography} from "@mui/material";
 
 export default function NavBar() {
     return (
-        <AppBar color="warning" position="static">
+        <AppBar color="success" position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Recipe App

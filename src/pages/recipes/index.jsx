bcd87bac04ccc309d8d4da71baaa9fc8a09c0b2e
SADDA12 <sammiedaga@gmail.com>
2024-02-27 12:18:48 +0000

    Implemented enter key with onKeyDown event

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -4,11 +4,15 @@ import { useEffect, useState } from "react";
 
 export default function Recipes() {
     const [recipes, setRecipes] = useState([]);
+    const [searchItem, setSearchItem] = useState("");
 
     const searchRecipes = () => {
         // prepare url
         const url = new URL('https://api.spoonacular.com/recipes/complexSearch');
-        url.searchParams.append('apiKey', '4859290582554951b8e400bfec473cf4')
+        url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+        url.searchParams.append('query', searchItem);
+        // add the query parameter
+
         // fetch recipes
         fetch(url)
             .then((response) => { return response.json() })
@@ -30,7 +34,11 @@ export default function Recipes() {
                 fullWidth
                 id="outlined-basic"
                 label="Enter a keyword to search recipes and hit Enter"
-                variant="outlined" />
+                variant="outlined" 
+                value={searchItem}
+                onChange={(event) => setSearchItem(event.target.value)}
+                onKeyDown={event => event.key === 'Enter' && searchRecipes()}
+                />
 
             <Grid sx={{ mt: '1rem' }} container spacing={3}>
                 {recipes.map((recipe) => <RecipeItem key={recipe.id} title={recipe.title} image={recipe.image}/>)}
